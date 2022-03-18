6d19ba67298ef656da23abbee49cd6939a4de713
Fernando Ticona <thefersh24@gmail.com>
2022-03-17 20:20:22 -0400

    html:code mejorado

--- app/utils/renderBody.tsx
+++ app/utils/renderBody.tsx
@@ -5,9 +5,33 @@ import {
   JSXMapSerializer,
 } from "@prismicio/react";
 import Highlight from "react-highlight";
+import hlc from "highlight.js";
 
 const renderHelpers: JSXMapSerializer | JSXFunctionSerializer = {
-  preformatted: ({ children }) => <Highlight>{children}</Highlight>,
+  preformatted: (p) => {
+    const t = String(p.text);
+    if (t.startsWith("thefersh")) {
+      const re = /thefersh:(.*)\n/;
+      let language = "";
+      t.replace(re, function (match) {
+        language = match.replace(/thefersh:/, "").replace(/\n/, "");
+        return "";
+      });
+      const code = hlc.highlight(t.replace(/thefersh:(.*)\n/, ""), {
+        language,
+      }).value;
+
+      return (
+        <pre>
+          <code
+            className={"hljs language-" + language}
+            dangerouslySetInnerHTML={{ __html: code }}
+          />
+        </pre>
+      );
+    }
+    return <Highlight>{p.children}</Highlight>;
+  },
 };
 
 export function RenderBody({ render }: { render: any }): JSX.Element {
