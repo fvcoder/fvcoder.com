fa2c36635076025ee0d2eae0dc40745b6e0fe863
Fernando Ticona <thefersh24@gmail.com>
2022-04-05 19:43:51 -0400

    youtube videos iframe

--- app/utils/renderBody.tsx
+++ app/utils/renderBody.tsx
@@ -17,6 +17,21 @@ const renderHelpers: JSXMapSerializer | JSXFunctionSerializer = {
     const e = CodeByLangHelper({ t }) || LinkByCodeHelper({ t });
     return e || <Highlight>{p.children}</Highlight>;
   },
+  embed: (p) => {
+    const id = new URL(p.node.oembed.embed_url).pathname.replace("/", "");
+    return (
+      <div className="w-full relative mb-4  " style={{ paddingTop: "56.25%" }}>
+        <iframe
+          className="inset-0 absolute w-full h-full rounded-md shadow"
+          src={`https://www.youtube.com/embed/${id}`}
+          title={p.node.oembed.title as string}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+    );
+  },
 };
 
 export function RenderBody({ render }: { render: any }): JSX.Element {
