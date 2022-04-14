747e2baaa317c5380527fa83b85da693ff4e39ba
Fernando Ticona <thefersh24@gmail.com>
2022-04-14 09:55:56 -0400

    fix: paginacion

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -22,28 +22,26 @@ export const Pagination: FC<PaginationProps> = ({ size, page, url }) => {
           <span>Anterior</span>
         </Link>
       )}
-      {Array.from({ length: size }).map((_, i) => {
-        if (page === i + 1) {
-          return (
-            <div
-              key={`pagination-${i}`}
-              className={
-                "py-2 px-3 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-slate-800 dark:border-slate-700 dark:text-slate-400 select-none " +
-                (page === 1 ? " rounded-l-lg" : "")
-              }
-            >
-              {page}
-            </div>
-          );
+
+      <div
+        className={
+          "py-2 px-3 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 dark:bg-slate-800 dark:border-slate-700 dark:text-slate-400 select-none " +
+          (page === 1 ? " rounded-l-lg" : "") +
+          (page === size ? " rounded-r-lg" : "")
         }
-      })}
-      <Link
-        to={`${urlBase}?page=${page + 1}`}
-        className="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white flex items-center"
       >
-        <span>Siguiente</span>
-        <ChevronRightIcon className="w-5 h-5" />
-      </Link>
+        {page}
+      </div>
+
+      {size !== page ? (
+        <Link
+          to={`${urlBase}?page=${page + 1}`}
+          className="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white flex items-center"
+        >
+          <span>Siguiente</span>
+          <ChevronRightIcon className="w-5 h-5" />
+        </Link>
+      ) : null}
     </div>
   );
 };
