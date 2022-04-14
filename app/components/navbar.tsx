98ef809efb810defe2d2dc209af56b5e07b16059
Fernando Ticona <thefersh24@gmail.com>
2022-04-14 10:35:51 -0400

    delete about

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -25,12 +25,6 @@ export function Navbar(): JSX.Element {
           Fernando Ticona
         </span>
       </Link>
-      <Link
-        to="/about"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        Acerca de
-      </Link>
     </nav>
   );
 }
