fee0f3587836587195e720747796006dcfc84e1c
Fernando Ticona <thefersh24@gmail.com>
2022-05-26 08:58:09 -0400

    tag Page

--- app/prismic/blog.list.ts
+++ app/prismic/blog.list.ts
@@ -31,3 +31,29 @@ export async function getBlogList(page = 1) {
     pageSize: data.total_pages
   }
 }
+
+export async function getBlogListByTag(tag: string, page = 1) {
+  const items = await client.getByTag(tag, {
+    orderings: {
+      direction: 'desc',
+      field: 'document.last_publication_date'
+    },
+    page,
+    pageSize: 12
+  })
+
+  return {
+    data: items.results.map(x => {
+      return {
+        uid: x.uid as string,
+        title: x.data.title[0].text as string,
+        image: x.data.image.url as string,
+        imageAlt: x.data.image.alt as string,
+        description: x.data.description[0].text as string,
+        tags: x.tags,
+        lastPublicationDate: dateFormat(x.last_publication_date)
+      }
+    }),
+    pageSize: items.total_pages
+  }
+}
