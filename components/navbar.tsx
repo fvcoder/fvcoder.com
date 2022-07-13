a51c6f4357b77bcb853ccf869ee757c25ab83c92
Fernando Ticona <thefersh24@gmail.com>
2022-07-13 09:17:23 -0400

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,35 @@
+import Link from 'next/link'
+import Img from 'next/image'
 import { NavbarContainer } from '../styles/navbar.style'
+import LogoImg from '../public/android-icon-48x48.png'
+import { styled } from '../stitches.config'
+
+const Logo = styled('a', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '.5rem',
+  textDecoration: 'none',
+  '> span': {
+    fontFamily: '$sans',
+    fontSize: '1rem',
+    color: '$gray12'
+  }
+})
 
 export function Navbar(): JSX.Element {
-  return <NavbarContainer>d</NavbarContainer>
+  return (
+    <NavbarContainer>
+      <Link href="/" passHref>
+        <Logo>
+          <Img
+            src={LogoImg}
+            alt="Fernando Ticona logo"
+            width={35}
+            height={35}
+          />
+          <span>Fernando Ticona</span>
+        </Logo>
+      </Link>
+    </NavbarContainer>
+  )
 }
--- stitches.config.js
+++ stitches.config.js
@@ -1,7 +1,11 @@
 import { createStitches } from '@stitches/react'
+import { gray } from '@radix-ui/colors'
 
 export const { styled, getCssText, globalCss } = createStitches({
   theme: {
+    colors: {
+      ...gray
+    },
     fonts: {
       sans: '"Open Sans", sans-serif',
       montserrat: 'Montserrat, system-ui'
