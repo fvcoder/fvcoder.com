5720110b6c4271c297aa92a9ce18422f691c9177
Fernando Ticona <thefersh24@gmail.com>
2022-07-13 07:47:24 -0400

    no me arrepentireeeeeeeeeeeeeee...

--- app/types/article.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import type { PrismicDocumentMeta } from './blog'
-
-export interface ArticleDocument
-  extends Omit<PrismicDocumentMeta, 'description'> {
-  data: any
-  tags: string[]
-}
--- workbox-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: 'public/',
-  globPatterns: ['**/*.{png,ico,pdf,eot,ttf,woff,woff2,svg,css}'],
-  swDest: 'public/sw.js',
-  swSrc: 'pwa/sw.js'
-}
