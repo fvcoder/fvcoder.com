fe33000530953530262cc9b77b51bcfe57e74e79
Fernando Ticona <thefersh24@gmail.com>
2022-10-03 19:05:31 -0400

    add blog index page

--- app/feactures/navbar.tsx
+++ app/feactures/navbar.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { Link } from "@remix-run/react"
 import classNames from 'classnames'
 import { socialNetwork } from "~/data/newtwork.data"
-import LogoImg from "./../../public/android-icon-48x48.png"
+import { Logo } from "~/components/logo"
 
 export function Navbar(): JSX.Element {
   const [open, setOpen] = useState(false)
@@ -11,13 +11,7 @@ export function Navbar(): JSX.Element {
     <div className="container px-6 py-4 mx-auto">
         <div className="lg:flex lg:items-center">
             <div className="flex items-center justify-between">
-                <div>
-                    <Link to="/" className="flex items-center gap-2 font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300 select-none">
-                      <img src={LogoImg} alt="thefersh logo" width={35} height={35} />
-                      <span className="text-base">Fernando Ticona</span>
-                    </Link>
-                </div>
-
+                <Logo />
                 <div className="flex lg:hidden">
                     <button onClick={() => setOpen(!open)} type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
                       {open ? (<svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,9 +1,36 @@
-import { Navbar } from "~/feactures/navbar";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { Pagination } from "~/components/pagination";
+import type { getBlogListR } from "~/prismic/blog.list";
+import { getBlogList } from "~/prismic/blog.list";
 
-export default function BlogPage() {
+export interface BlogPageLoader {
+  list: getBlogListR['data']
+  page: number
+  totalPages: number
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url)
+  const page = url.searchParams.has('page')
+    ? Number.isNaN(url.searchParams.get('page'))
+      ? 1
+      : Number(url.searchParams.get('page'))
+    : 1
+
+  const blogs = await getBlogList(page)
+  return json<BlogPageLoader>({
+    list: blogs.data,
+    page,
+    totalPages: blogs.pageSize
+  })
+}
+
+export default function BlogPage(): JSX.Element {
+  const { list, page, totalPages } = useLoaderData<BlogPageLoader>()
   return (
     <>
-      <Navbar />
       <section className=" dark:bg-gray-900">
         <div className="container px-6 py-10 mx-auto">
             <div className="text-center">
@@ -14,42 +41,27 @@ export default function BlogPage() {
             </div>
 
             <div className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
-                <div>
-                    <img className="relative z-10 object-cover w-full rounded-md h-96" src="https://images.unsplash.com/photo-1644018335954-ab54c83e007f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+              {list.map((x, i) => (
+                <div key={`blog-list-${i}`}>
+                    <img className="relative z-10 object-cover w-full rounded-md h-96" src={x.image} alt={x.imageAlt || x.title} />
 
                     <div className="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-gray-900">
-                        <a href="/" className="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
-                            All the features you want to know
-                        </a>
+                        <Link to={`/blog/${x.uid}`} className="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
+                            {x.title}
+                        </Link>
 
                         <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis sint autem nesciunt,
-                            laudantium quia tempore delect
+                            {x.description}
                         </p>
 
-                        <p className="mt-3 text-sm text-blue-500">21 October 2019</p>
+                        <p className="mt-3 text-sm text-blue-500">{x.lastPublicationDate  }</p>
                     </div>
                 </div>
-
-                <div>
-                    <img className="relative z-10 object-cover w-full rounded-md h-96" src="https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
-
-                    <div className="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow dark:bg-gray-900">
-                        <a href="/" className="font-semibold text-gray-800 hover:underline dark:text-white md:text-xl">
-                            How to use sticky note for problem solving
-                        </a>
-
-                        <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis sint autem nesciunt,
-                            laudantium quia tempore delect
-                        </p>
-
-                        <p className="mt-3 text-sm text-blue-500">20 October 2019</p>
-                    </div>
-                </div>
+              ))}
             </div>
         </div>
     </section>
-    </>
+    <Pagination page={page} pageSize={totalPages} />
+  </>
   );
 }
