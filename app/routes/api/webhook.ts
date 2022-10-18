87da870a9980b4a8af9252a7c2e9e917e693d1e1
Fernando Ticona <fer@thefersh.com>
2022-10-18 17:49:35 -0400

    retiro 404 y añado variable a .env.example

--- app/routes/api/webhook.ts
+++ app/routes/api/webhook.ts
@@ -5,26 +5,27 @@ import { res404 } from "~/utils/response.404";
 
 interface prismicWebhook {
   type: string
-  masterRef: 'Y06rOhAAAPo9dIbz',
+  masterRef: string,
   documents: string[],
   domain: string
   apiUrl: string
   secret: string
 }
 
-export const loader: LoaderFunction = async({ params, request }) => {
+export const loader: LoaderFunction = () => {
   res404()
 }
 
 
-export const action: ActionFunction = async({ params, request }) => {
+export const action: ActionFunction = async({ request }) => {
   const url = new URL(request.url)
   let d: prismicWebhook = await request.json()
-  if (!d.secret || d.secret !== process.env.PRISMIC_WEBHOOK_SECRET) res404()
+  if (!d.secret || d.secret !== process.env.PRISMIC_WEBHOOK_SECRET) return json({})
 
   const doc = await client.getByID(d.documents[0])
-  if (!doc) res404()
+  if (!doc) return json({})
+  if (doc.type !== 'blog') return json({})
 
-  await fetch(`https://maker.ifttt.com/trigger/newArticlePost/with/key/${process.env.IFTTT_WEBHOOK_KEY}?value1=${url.origin}/blog/${doc.uid}`)
+  await fetch(`https://maker.ifttt.com/trigger/${process.env.IFTTT_WEBHOOK_EVENT}/with/key/${process.env.IFTTT_WEBHOOK_KEY}?value1=${url.origin}/blog/${doc.uid}`)
   return json({})
 }
