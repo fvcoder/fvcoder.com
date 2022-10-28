b32e6aa0eff98a85cba44ad965971050331fc209
Fernando Ticona <fer@thefersh.com>
2022-10-27 20:46:05 -0400

    project section

--- app/components/section/project.tsx
+++ app/components/section/project.tsx
@@ -13,23 +13,21 @@ export function ProjectSection({ data, notSeeALl }: ProjectSectionProps): JSX.El
         <div className="container mx-auto">
           <TitleSection title={"🙌 My Projects"} subTitle={"I believe there is much to create"} />
             <div className="py-6 mx-auto">
-              {data.map((x, i) => {
-                return (
-                  <div className="grid grid-cols-1 gap-8 md:grid-cols-2" key={`project-index-${i}`}>
-                      <div className="lg:flex">
-                          <img className="object-cover w-full h-56 rounded-lg lg:w-64" src={x.logo} alt={x.logoAlt ?? x.title} />
-
-                          <div className="flex flex-col justify-between py-6 lg:mx-6">
-                              <Link to={`/project/${x.uid}`} className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
-                                  {x.title}
-                              </Link>
-
-                              <span className="text-sm text-gray-500 dark:text-gray-300">{x.lastPublicationDate}</span>
-                          </div>
-                      </div>
-                  </div>
-                )
-              })}
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                {data.map((x, i) => {
+                  return (
+                    <Link to={`/project/${x.uid}`} className="flex items-center gap-4" key={`project-index-${i}`}>
+                        <img className="object-cover rounded-lg w-12 h-12 lg:w-1/4 lg:h-auto aspect-square" src={x.logo} alt={x.logoAlt ?? x.title} />
+                        <div className="flex flex-col justify-between py-2 lg:mx-4">
+                            <Link to={`/project/${x.uid}`} className="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
+                                {x.title}
+                            </Link>
+                            <span className="text-sm text-gray-500 dark:text-gray-300">{x.lastPublicationDate}</span>
+                        </div>
+                    </Link>
+                  )
+                })}
+              </div>
               {!notSeeALl && <Link to="/project" className="text-sm text-gray-500 dark:text-gray-300 py-4 block w-fit ml-auto hover:underline">See All -&gt;</Link>}
             </div>
         </div>
