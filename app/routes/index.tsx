e1001283c65f4152e62d72fa56ddc36a744142b4
Fernando Ticona <fer@thefersh.com>
2022-11-01 09:32:00 -0400

    limitado a 3

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -25,7 +25,7 @@ interface IndexLoader {
 export const loader: LoaderFunction = async ({request}) => {
   const blog = (await getBlogList(1)).data
   const project = (await getProjectList(1)).data.slice(0, 4)
-  const testimonial = (await getTestimonialList()).data
+  const testimonial = (await getTestimonialList()).data.slice(0, 3)
   return json<IndexLoader>({ blog, project, testimonial, url: request.url.split("?")[0] })
 }
 
