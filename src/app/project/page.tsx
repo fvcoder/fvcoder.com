ef7cb496a545ea22b25a626f72e9afb9dc4c70d4
Fernando Ticona <thefersh24@gmail.com>
2024-04-19 10:19:30 -0400

    feat: :sparkles: project skill list

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,5 +1,14 @@
 import { prisma } from '@/features/core/lib/prisma.server';
+import { getMetadata } from '@/features/core/utils/metadata';
 import ProjectHome from '@/features/projects/page/home';
+
+export function generateMetadata() {
+  return getMetadata({
+    title: 'Habilidades de Fernando Ticona | fvcoder',
+    description:
+      'Explora mis habilidades en fvcoder, en diferentes tecnologías y recursos como: JavaScript, TypeScript, React, Next.js, Tailwind',
+  });
+}
 
 export default async function ProjectHomePage() {
   const skills = await prisma.skills.findMany({
