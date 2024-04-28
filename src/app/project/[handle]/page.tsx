e2dceb4c7f1d9a20ab5a4c3d4561ec68d4c985cc
Fernando Ticona <thefersh24@gmail.com>
2024-04-28 09:16:31 -0400

    feat(projects): :sparkles: renderizado de pagina de proeyectos metadatos

--- src/app/project/[handle]/page.tsx
+++ src/app/project/[handle]/page.tsx
@@ -1,79 +1,69 @@
-import { Icon } from '@iconify-icon/react/dist/iconify.mjs';
-import { Button } from '@nextui-org/react';
-import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
-import { Footer } from '@/components/footer';
-import { Container } from '@/features/core/components/container';
-import { EditorRender } from '@/features/core/components/editor.render';
 import { prisma } from '@/features/core/lib/prisma.server';
+import { getMetadata } from '@/features/core/utils/metadata';
+import { ProjectViewPage } from '@/features/projects/page/project.view';
+import { PageProps } from '@/types/next.page';
 
-export default async function ProjectDetailPage() {
-  const skills = await prisma.skills.findMany({
-    select: {
-      handle: true,
-      name: true,
-      icon: true,
-      color: true,
-    },
-    take: 5,
-    orderBy: { authority: 'desc' },
-  });
+export async function generateMetadata({ params }: PageProps) {
+  try {
+    const article = await prisma.project.findFirst({
+      select: {
+        name: true,
+        description: true,
+        thumbnail: true,
+        handle: true,
+      },
+      where: {
+        handle: params.handle,
+        state: 'published',
+      },
+    });
 
-  return (
-    <Container className="prose dark:prose-invert py-10" fullWidth>
-      <header className="text-center pb-4">
-        <span className="block mb-4">
-          <time>17 de abril de 2024</time>
-        </span>
-        <div>
-          <h1>
-            Así es como aprendí a programar, mi plan de estudios + Cursos Gratis
-          </h1>
-          <div className="aspect-video bg-black rounded-md"></div>
-        </div>
-      </header>
-      <article>
-        <EditorRender
-          content={{
-            blocks: [
-              { type: 'paragraph', id: '1', data: { text: 'Primer párrafo' } },
-            ],
-          }}
-        />
-        <EditorRender
-          content={{
-            blocks: [
-              { type: 'paragraph', id: '2', data: { text: 'Primer párrafo' } },
-            ],
-          }}
-        />
-        <div className="">
-          <p>
-            <strong>Construido con:</strong>
-          </p>
-          <div className="not-prose flex gap-2 flex-wrap pb-4">
-            {skills.map((x, i) => (
-              <Button
-                as={Link}
-                href={`/skill/${x.handle}`}
-                size="sm"
-                variant="bordered"
-                className="flex rounded-full pl-2"
-                style={{
-                  color: x.color,
-                  borderColor: x.color,
-                  background: 'transparent',
-                }}
-                startContent={<Icon icon={x.icon} width={18} height={18} />}
-                key={`skill-${x.handle}-${i}`}
-              >
-                {x.name}
-              </Button>
-            ))}
-          </div>
-        </div>
-      </article>
-      <Footer />
-    </Container>
-  );
+    if (!article) {
+      return getMetadata({ title: '404 | Project not found' });
+    }
+
+    return getMetadata({
+      title: `${article.name} | Proyecto de Fernando Ticona | fvcoder`,
+      alternates: {
+        canonical: `/project/${article.handle}`,
+      },
+      description: article.description,
+      openGraph: {
+        images: article.thumbnail,
+      },
+      twitter: {
+        images: article.thumbnail,
+      },
+    });
+  } catch {
+    return getMetadata({ title: '404 | Project not found' });
+  }
+}
+
+export default async function ProjectPageServer(props: PageProps) {
+  try {
+    if (!props.params.handle) {
+      notFound();
+    }
+
+    const article = await prisma.project.findFirst({
+      where: {
+        handle: props.params.handle,
+        state: 'published',
+      },
+      include: {
+        skills: true,
+      },
+    });
+
+    if (!article) {
+      notFound();
+    }
+
+    return <ProjectViewPage {...article} />;
+  } catch {
+    notFound();
+  }
 }
