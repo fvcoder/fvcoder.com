58a5859ab8cac8c3e90247e0428eaa2fac96ae53
Fernando Ticona <thefersh24@gmail.com>
2026-04-21 20:32:26 -0400

    Refactor code structure for improved readability and maintainability

--- src/pages/og/blog/[slug].png.ts
+++ src/pages/og/blog/[slug].png.ts
@@ -2,14 +2,17 @@ import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 import { ImageResponse } from '@vercel/og';
 import { html } from 'satori-html';
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 
 export async function getStaticPaths() {
   const posts = await getCollection('blog');
   
   return posts.map((post) => ({
     params: { slug: post.id },
-    props: { 
+    props: {
       title: post.data.title,
+      slug: post.id,
       dateText: post.data.pubDate.toLocaleDateString('es-MX', {
         year: 'numeric',
         month: 'long',
@@ -20,31 +23,53 @@ export async function getStaticPaths() {
 }
 
 export const GET: APIRoute = async ({ props }) => {
-  const markup = html`
-    <div tw="flex flex-col w-full h-full bg-white px-20 py-8">
-      <div tw="flex items-center justify-between">
-        <p tw="relative text-xl text-zinc-500 mb-0">Articulo</p>
-        <p tw="relative text-xl text-zinc-500 mb-0">${props.dateText}</p>
-      </div>
-      <div tw="flex flex-1 flex-col items-start">
-        <h1 tw="text-balance relative text-6xl font-bold text-left leading-tight">
-          ${props.title}
-        </h1>    
-      </div>
-      <div tw="flex items-center justify-between">
-        <div tw="flex items-center">
-            <p tw="relative text-2xl text-zinc-500">Fernando Ticona</p>
-        </div>
+  const fontMonoData = readFileSync(resolve(process.cwd(), 'src/assets/fonts/JetBrainsMono-Regular.ttf')).buffer;
+  const fontInterData = readFileSync(resolve(process.cwd(), 'src/assets/fonts/Inter_24pt-Regular.ttf')).buffer;
 
-        <p tw="relative text-2xl text-zinc-500 mt-10">
-          Articulo
-        </p>
+
+  const markup = html`
+    <div tw="flex flex-col w-full h-full sbg-[#030542]"
+    style="background-image: linear-gradient(to top, #eac8ff, #be9fd8, #9378b3, #6a538f, #41316c, #2f235d, #1b154f, #040641, #040641, #040641, #040641, #040641);"
+    >
+      <div tw="flex flex-1 flex-col px-12 pt-28">
+        <div tw="flex text-white/50 mx-auto text-2xl">Blog de Fernando Ticona</div>
+        <div tw="flex flex-1 flex-col items-start">
+          <h1 tw="relative text-5xl font-bold leading-tight text-white text-center w-3/4 mx-auto">
+            ${props.title}
+          </h1>    
+          <div tw="flex text-white/50 mx-auto text-xl">${props.dateText}</div>
+        </div>
+      </div>
+      <div tw="flex items-center justify-center">
+        <div
+          tw="h-48 bg-[#ffffff26] border-t-[.0625rem] border-r-[.0625rem] border-l-[.0625rem] border-[#8c93fb] flex w-4/5 rounded-t-3xl pt-6 px-6"
+        >
+          <div tw="h-full bg-[#151c23] flex rounded-t-xl w-full">
+            <div tw="font-mono text-lg flex text-[#84a6ce] p-6" style="font-family: 'mono';">
+              https://fvcoder.com/blog/${props.slug}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   `;
   return new ImageResponse(markup as any, {
     width: 1200,
     height: 630,
+    fonts: [
+      {
+        name: 'MiFuenteInter',
+        data: fontInterData,
+        weight: 400,
+        style: 'normal',
+      },
+      {
+        name: 'mono',
+        data: fontMonoData,
+        weight: 400,
+        style: 'normal',
+      },
+    ],
   });
 
   
